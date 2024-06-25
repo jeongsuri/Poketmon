ef840974aed5d6e7e08e68e13f345503bbebf085
jeongsuri <jeongsuri96@gmail.com>
2024-06-25 18:38:36 +0900

    초기설정

--- src/main/java/org/choongang/board/controllers/BoardController.java
+++ src/main/java/org/choongang/board/controllers/BoardController.java
@@ -3,6 +3,7 @@ package org.choongang.board.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Controller;
+import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.RequestMapping;
 
 @Controller
@@ -10,8 +11,11 @@ import org.choongang.global.config.annotations.RequestMapping;
 @RequiredArgsConstructor
 public class BoardController {
 
+    //게시판 양식
     @RequestMapping("/boarding")
     public String boarding() {
-        return "boarding";
+        return "/board/boarding";
     }
+
+
 }
