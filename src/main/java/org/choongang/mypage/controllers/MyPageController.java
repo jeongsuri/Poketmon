ab58a81827be83cef72cf747abe70cc4eef743ab
yeonhwa001 <chyh202@naver.com>
2024-06-25 18:31:53 +0900

    .

--- src/main/java/org/choongang/mypage/controllers/MyPageController.java
+++ src/main/java/org/choongang/mypage/controllers/MyPageController.java
@@ -2,11 +2,16 @@ package org.choongang.mypage.controllers;
 
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.game.controllers.GameController;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.RequestMapping;
+import org.choongang.member.controllers.MemberController;
 
 @Controller
 @RequestMapping("/mypage")
 @RequiredArgsConstructor
-public class MyPageController {
+public class MyPageController extends GameController {
+
+
+
 }
