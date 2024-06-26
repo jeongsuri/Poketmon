a1926f3bf668381f6fa88b53b6ff6f2853915e09
sunkyu0384 <sunkyu0384@naver.com>
2024-06-26 17:42:03 +0900

    순위표 화면 구성

--- src/main/java/org/choongang/rank/controllers/RankController.java
+++ src/main/java/org/choongang/rank/controllers/RankController.java
@@ -1,12 +1,24 @@
 package org.choongang.rank.controllers;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/rank")
 @RequiredArgsConstructor
 public class RankController {
+    private final HttpServletRequest request;
+
+    @RequestMapping("/ranking")
+    public String ranking() {
+        String url = request.getRequestURI();
+        request.setAttribute("addCss", List.of("rank"));
+
+        return "/rank/ranking";
+    }
 }
--- src/main/webapp/WEB-INF/templates/rank/ranking.jsp
+++ src/main/webapp/WEB-INF/templates/rank/ranking.jsp
@@ -4,4 +4,19 @@
 <%@ taglib prefix="layout" tagdir="/WEB-INF/tags/layouts" %>
 <fmt:setBundle basename="messages.commons" />
 <fmt:message var="pageTitle" key='순위표' />
-<c:url var="actionUrl" value="/rank/ra" />
+<c:url var="actionUrl" value="/rank" />
+<c:url var="cssUrl" value="/css/" />
+
+
+<layout:main title="${pageTitle}">
+    <section class="content-box">
+        <h1>${pageTitle}</h1>
+        <div class="search">
+        <input class="trainer_search" type="text" placeholder="아이디 검색">
+        <button class="search_btn">검색</button>
+        </div>
+    </section>
+</layout:main>
+
+
+
