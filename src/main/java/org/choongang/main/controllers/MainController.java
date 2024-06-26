681fa31c39ee382466d2c8d3a44c8fcb4681e998
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-26 09:01:55 +0900

    MainController

--- src/main/java/org/choongang/main/controllers/MainController.java
+++ src/main/java/org/choongang/main/controllers/MainController.java
@@ -3,10 +3,16 @@ package org.choongang.main.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Controller;
+import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.RequestMapping;
 
 @Controller
 @RequestMapping("/main")
 @RequiredArgsConstructor
 public class MainController {
+
+    @GetMapping
+    public String main() {
+        return "main";
+    }
 }
