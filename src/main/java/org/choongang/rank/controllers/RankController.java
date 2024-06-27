d64fea4aa5d0395e32f03c0ada066d66b3578232
sunkyu0384 <sunkyu0384@naver.com>
2024-06-27 13:54:13 +0900

    순위표 페이지 연결, 디자인 구성

--- src/main/java/org/choongang/rank/controllers/RankController.java
+++ src/main/java/org/choongang/rank/controllers/RankController.java
@@ -4,6 +4,7 @@ package org.choongang.rank.controllers;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Controller;
+import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.RequestMapping;
 
 import java.util.List;
@@ -14,11 +15,11 @@ import java.util.List;
 public class RankController {
     private final HttpServletRequest request;
 
-    @RequestMapping("/ranking")
+    @GetMapping
     public String ranking() {
         String url = request.getRequestURI();
         request.setAttribute("addCss", List.of("rank"));
 
-        return "/rank/ranking";
+        return "rank/ranking";
     }
 }
