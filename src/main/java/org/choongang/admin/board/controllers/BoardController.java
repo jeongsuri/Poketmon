c7a25b0a76c6b2e187ed04a278389404ea1b1308
jeongsuri <jeongsuri96@gmail.com>
2024-06-27 16:30:09 +0900

    admin페이지 구성

--- src/main/java/org/choongang/admin/board/controllers/BoardController.java
+++ src/main/java/org/choongang/admin/board/controllers/BoardController.java
@@ -1,8 +1,12 @@
 package org.choongang.admin.board.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/board")
@@ -11,7 +15,6 @@ public class BoardController {
     // 게시판 목록
     @GetMapping
     public String boardList() {
-
         return "admin/board/index";
     }
 
