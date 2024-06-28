969b114151c550d966bf96c535ceab4ac251b47c
sunkyu0384 <sunkyu0384@naver.com>
2024-06-28 09:14:25 +0900

    순위표 서비스 구성

---
+++ src/main/java/org/choongang/rank/services/RankService.java
@@ -0,0 +1,10 @@
+package org.choongang.rank.services;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.global.config.annotations.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RankService {
+
+    }
