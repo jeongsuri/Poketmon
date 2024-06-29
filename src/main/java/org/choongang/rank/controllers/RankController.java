5043bb47cfa75e9d02c4ef756520cef44c78f50e
sunkyu0384 <sunkyu0384@naver.com>
2024-06-30 04:51:39 +0900

    순위표 아이디 검색 결과 페이지 구성

--- src/main/java/org/choongang/rank/controllers/RankController.java
+++ src/main/java/org/choongang/rank/controllers/RankController.java
@@ -3,9 +3,7 @@ package org.choongang.rank.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.choongang.global.config.annotations.Controller;
-import org.choongang.global.config.annotations.GetMapping;
-import org.choongang.global.config.annotations.RequestMapping;
+import org.choongang.global.config.annotations.*;
 import org.choongang.rank.entities.Rank;
 import org.choongang.rank.mapper.RankMapper;
 
@@ -21,6 +19,7 @@ public class RankController {
     @GetMapping
     public String rank() {
         List<Rank> rankingList = rankMapper.getRanking();
+
         request.setAttribute("rankingList", rankingList);
         request.setAttribute("addCss", List.of("rank"));
 
@@ -28,12 +27,12 @@ public class RankController {
     }
 
     @GetMapping("/search")
-    public String search(String userId) {
+    public String search(@RequestParam("userId") String userId) {
         Rank userRank = rankMapper.getUserRank(userId);
         request.setAttribute("userRank", userRank);
         request.setAttribute("addCss", List.of("rank"));
 
-        return "rank/searchResult";
+        return "rank/search";
     }
 }
 
