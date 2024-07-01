7bf3fb2c7c60784965af7c4c7a46b53df17bb9a7
jeongsuri <jeongsuri96@gmail.com>
2024-07-01 10:10:28 +0900

    게시판 작성, 수정 검증구현

---
+++ src/main/java/org/choongang/board/services/BoardSaveService.java
@@ -0,0 +1,51 @@
+package org.choongang.board.services;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.choongang.board.controllers.RequestBoardData;
+import org.choongang.board.entities.BoardData;
+import org.choongang.board.mapper.BoardDataMapper;
+import org.choongang.board.validators.BoardSaveValidator;
+import org.choongang.global.config.annotations.Service;
+import org.choongang.global.config.containers.BeanContainer;
+import org.choongang.member.MemberUtil;
+import org.modelmapper.ModelMapper;
+
+/**
+ * 기시글 추가, 수정
+ */
+@Service
+@RequiredArgsConstructor
+public class BoardSaveService {
+
+    private final BoardDataMapper mapper;
+    private final BoardSaveValidator validator;
+    private final MemberUtil memberUtil;
+
+    public void save(RequestBoardData form){
+        validator.check(form);
+        String mode = form.getMode();
+        mode = mode == null || mode.isBlank() ? "write" : mode;
+
+        BoardData data = new ModelMapper().map(form, BoardData.class);
+
+        if(mode.equals("update")){//수정
+
+        }else {//등록
+            //현재 로그인한 회원번호
+            if (memberUtil.isLogin()) {
+                data.setMemberSeq(memberUtil.getMember().getUserNo());
+            }
+
+            //작성자 환경 정보
+            HttpServletRequest request = BeanContainer.getInstance().getBean(HttpServletRequest.class);
+            String ua = request.getHeader("User-Agent"); //작성자 브라우저 정보
+            String ip = request.getRemoteAddr(); //작성자 IP정보
+            data.setUa(ua);
+            data.setIp(ip);
+        }
+
+
+
+    }
+}
