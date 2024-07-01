2c47e7c53956c48123e175701727d2109799a73d
parksaehyun <psh666777@naver.com>
2024-07-01 10:41:15 +0900

    TDD 구성중

--- src/test/java/org/choongang/member/tests/JoinServiceTest.java
+++ src/test/java/org/choongang/member/tests/JoinServiceTest.java
@@ -1,4 +1,28 @@
 package org.choongang.member.tests;
 
+import lombok.RequiredArgsConstructor;
+import org.choongang.global.config.annotations.Service;
+import org.choongang.member.entities.Member;
+import org.choongang.member.mapper.MemberMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Service
+@RequiredArgsConstructor
+@DisplayName("회원가입 기능 테스트")
 public class JoinServiceTest {
+
+    private Member member;
+    private MemberMapper memberMapper;
+
+    @Test
+    @DisplayName("회원가입 성공 시 예외 발생하지 않음")
+    void successTest() {
+        assertDoesNotThrow(() -> {
+
+        });
+    }
+
 }
